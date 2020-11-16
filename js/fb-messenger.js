872f918ad048574c1eeff6db7dd687614c33e990
dpw1 <diego.boarutto.fortes@gmail.com>
2020-11-15 21:00:29 -0300

    Updates

--- js/fb-messenger.js
+++ js/fb-messenger.js
@@ -14,4 +14,4 @@ setTimeout(() => {
     js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js"
     fjs.parentNode.insertBefore(js, fjs)
   })(document, "script", "facebook-jssdk")
-}, 20000)
+}, 3000)
