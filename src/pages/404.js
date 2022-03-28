4198c01e8f5951d753ea297dd7a9d648f2424207
dpw1 <diego.boarutto.fortes@gmail.com>
2022-03-28 03:48:07 -0300

    [FIX] 404 page design, redirect to homepage

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,7 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
+import "./404.css"
 
 const FourZeroFour = props => {
-  return <p>This page does not exist.</p>
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.href = `https://ezfycode.com`
+    }, 2000)
+  }, [])
+
+  return (
+    <p className="FourZeroFour">
+      Oops! This page does not exist. Redirecting...
+    </p>
+  )
 }
 
 export default FourZeroFour
