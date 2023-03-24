a62e235bf1aaf871e51d7e7e8dedd128735303c8
dpw1 <diego.boarutto.fortes@gmail.com>
2023-03-24 13:30:11 +0000

    Updates

--- js/gtag.js
+++ js/gtag.js
@@ -1,3 +1,11 @@
+// window.dataLayer = window.dataLayer || []
+// function gtag() {
+//   dataLayer.push(arguments)
+// }
+// gtag("js", new Date())
+
+// gtag("config", "G-9MMTLWEN98")
+
 window.dataLayer = window.dataLayer || []
 function gtag() {
   dataLayer.push(arguments)
